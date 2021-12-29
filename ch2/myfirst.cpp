f13b69a605832ed337ae1a73b6798ce914da9b80
kk <mingli>
2021-12-29 23:07:56 +0800

    myfirst.cpp

---
+++ ch2/myfirst.cpp
@@ -0,0 +1,12 @@
+//
+// Created by kk on 2021/12/29.
+//
+#include <iostream>
+int ch2myfirst()
+{
+    using namespace std;
+    cout<<"come up and C++ me some time";
+    cout<<endl;
+    cout<<"You won't regret it!";
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "ch2/myfirst.cpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //ch2 exercise
+    ch2myfirst();
+
+//    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
